c4e657b16c1f8954c665974d26c96524474bf5eb
Arif Rachman Husen <arif.rachman@sicepat.com>
2021-10-08 13:02:43 +0700

    adding bash_url response

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,10 @@
 "use strict";
+const { response } = require("express");
 const express = require("express");
 const router = express.Router();
+router.get("/", (req, res) => {
+  res.status(200).json("API of Wedding Putri and Arif ");
+});
 const Wishes = require("./wishesRoute");
 router.use("/wishes", Wishes);
 

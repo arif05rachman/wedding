c11e279e26dd2d862a7f1d0040a821f9a41a521c
Arif Rachman Husen <arif.rachman@sicepat.com>
2021-10-08 15:39:08 +0700

    fix base url

--- client/src/constant.js
+++ client/src/constant.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://intense-bastion-30824.herokuapp.com/";
+const BASE_URL = "https://intense-bastion-30824.herokuapp.com";
 export const API_URL = {
   WISHES_URL: `${BASE_URL}/wishes`,
 };
